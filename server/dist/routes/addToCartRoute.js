89a1a3ded9bf093c80dad284e1cc29d5a0489bed
Alex <uzzy412@yahoo.com>
2025-09-08 14:27:05 +0300

    restarted backend

--- server/dist/routes/addToCartRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { addToCartController } from '../controllers/addToCartController.js';
-const router = express.Router();
-router.post('/', addToCartController);
-export default router;
